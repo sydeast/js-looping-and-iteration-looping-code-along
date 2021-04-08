20eccdae2ec78480cdf13bc9104daccf05929f76
Syd <24235464+sydeast@users.noreply.github.com>
2021-04-07 22:09:00 -0500

    commit tree 1e5304668c0ea4d182a261757d0d0c91258bf41e

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+function writeCards(names) {
+    let thankYou = [];
+    for (let i = 0; i < names.length; i++) {
+        thankYou.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`)
+    };
+    return thankYou;
+};
+
+
+function countDown(num) {
+    let i = 0;
+    while (i <= num) {
+        console.log(i++);
+    }
+    return num;
+}
